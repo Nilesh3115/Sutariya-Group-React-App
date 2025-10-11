1af9f21ca8d58f09acd90aaedaadc5ca0f1d43fb
Nilesh3115 <rn9319672@gmail.com>
2025-10-11 16:16:05 +0530

    About Us Created From Tailwind Done

--- src/components/About Us/AboutUs.jsx
+++ src/components/About Us/AboutUs.jsx
@@ -2,60 +2,65 @@ import "./AboutUs.css"
 
 const AboutUs = () => {
     return (
-        <section className="ab-works section-p1 section-mw 3xl:px-[150px] 3xl:py-[80px] 2xl:p-[80px] xl:py-[80px] xl:px-[60px] lg:py-[60px] lg:px-[40px] md:py-[40px] md:px-[20px] sm:py-[40px] sm:px-[20px] font-outfit bg-black text-white flex flex-col gap-[30px] justify-center items-center">
+        <section className="ab-works section-p1 section-mw 
+        3xl:px-[150px] 3xl:py-[80px] 2xl:p-[80px] xl:py-[80px] xl:px-[60px] 
+        lg:py-[60px] lg:px-[40px] md:py-[40px] md:px-[20px] 
+        sm:py-[40px] sm:px-[20px] 
+        font-outfit bg-black text-white 
+        flex flex-col justify-center items-center gap-[30px] w-full max-w-[1600px] ">
 
-            <div className="ab-work-para-wrap fade-in-up flex w-full items-start justify-center flex-wrap gap-x-[100px] gap-y-[25px] max-w-full mb-[40px]">
+            <div className="ab-work-para-wrap fade-in-up flex items-start justify-center flex-wrap max-w-full w-full sm:gap-x-[100px] sm:gap-y-[25px] mb-[40px] max-xl:justify-start max-xl:m-0  max-xl:flex-col">
                 {/* <!-- Section Title --> */}
-                <div className="ab-work-title flex items-center w-[46%]">
-                    <h2>We make your<br />bussiness stand out</h2>
+                <div className="ab-work-title flex items-center max:3xl:w-[45%] xl:w-[44%] lg:w-[60%] max-lg:w-[100%]">
+                    <h2 className="3xl:text-3xlh2 3xl:leading-3xlh2 2xl:text-2xlh2 2xl:leading-2xlh2 xl:text-xlh2 xl:leading-xlh2 lg:text-lgh2 lg:leading-lgh2 md:text-mdh2 md:leading-mdh2 sm:text-smh2 sm:leading-smh2">We make your<br />bussiness stand out</h2>
                 </div>
-                <div className="ab-work-para w-[46%] flex items-start justify-center flex-col">
-                    <p>Sutariya Group is dedicated to helping businesses and individuals shine with innovative solutions that combine creativity and technology. From IT education and digital services to custom invitations, printing, and marketing, we craft experiences that strengthen identity, build trust, and inspire growth. With a passion for excellence, our team ensures every service is designed to add value and make your business stand out in a competitive world.</p>
-                </div>
-            </div>
-
-            <div className="image-counter flex items-center justify-center max-w-full  gap-x-[100px] gap-y-[25px]">
-                <div className="image w-[50%] h-auto">
-                    <img className="m-w-full w-full h-auto" src="./images/68767a21fe51d132a838140b_round_tube.png" loading="lazy" alt="" />
-                </div>
-
-                <div className="counters w-[50%] flex items-center justify-center flex-wrap gap-[80px]">
-                    <div className="count-wrap fade-in-up w-[43%] items-center flex flex-col justify-center">
-                        <h3>329+</h3>
-                        <p>Project Complete</p>
-                    </div>
-
-                    <div className="count-wrap fade-in-up w-[43%] items-center flex flex-col justify-center">
-                        <h3>99%</h3>
-                        <p>Satisfied Clients</p>
-                    </div>
-
-                    <div className="count-wrap fade-in-up w-[43%] items-center flex flex-col justify-center">
-                        <h3>6240+</h3>
-                        <p>Hours of work</p>
-                    </div>
-
-                    <div className="count-wrap fade-in-up w-[43%] items-center flex flex-col justify-center">
-                        <h3>05+</h3>
-                        <p>Years Experience</p>
-                    </div>
+                <div className="ab-work-para max:3xl:w-[45%] xl:w-[44%] lg:w-[60%] max-lg:w-[100%] flex items-start justify-center flex-col">
+                    <p className="3xl:text-3xlp 3xl:leading-3xlp 2xl:text-2xlp 2xl:leading-2xlp xl:text-xlp xl:leading-xlp lg:text-lgp lg:leading-lgp md:text-mdp md:leading-mdp sm:text-smp sm:leading-smp">Sutariya Group is dedicated to helping businesses and individuals shine with innovative solutions that combine creativity and technology. From IT education and digital services to custom invitations, printing, and marketing, we craft experiences that strengthen identity, build trust, and inspire growth. With a passion for excellence, our team ensures every service is designed to add value and make your business stand out in a competitive world.</p>
                 </div>
             </div>
 
-            <div className="logo-images flex items-center justify-center gap-x-[32px] gap-y-[16px] py-[8px] flex-wrap">
-                <div className="images w-[17%]">
+            <div className="image-counter flex md:flex-row items-center justify-center max-w-full  max-lg:gap-y-[20] max-lg:gap-x-[40] gap-x-[100px] gap-y-[25px] sm:flex-col">
+                <div className="image w-[50%] sm:w-[100%] h-auto md:w-[50%]">
+                    <img className="max-w-full w-full h-auto" src="./images/68767a21fe51d132a838140b_round_tube.png" loading="lazy" alt="" />
+                </div>
+
+                <div className="counters w-[50%] sm:w-[100%] md:w-[50%] flex items-center justify-center flex-wrap gap-[80px] max-xl:gap-[40px]">
+                    <div className="count-wrap fade-in-up 3xl:w-[42%] 2xl:w-[42%] xl:w-[40%] lg:w-[43%] md:w-[40%] sm:w-[44%] max-xl:items-start items-center flex flex-col justify-center max-md:items-center">
+                        <h3 className="3xl:text-3xlh3 3xl:leading-3xlh3 2xl:text-2xlh3 2xl:leading-2xlh3 xl:text-xlh3 xl:leading-xlh3 lg:text-lgh3 lg:leading-lgh3 md:text-mdh3 md:leading-mdh3 sm:text-smh3 sm:leading-smh3">329+</h3>
+                        <p className="3xl:text-3xlp 3xl:leading-3xlp 2xl:text-2xlp 2xl:leading-2xlp xl:text-xlp xl:leading-xlp lg:text-lgp lg:leading-lgp md:text-mdp md:leading-mdp sm:text-smp sm:leading-smp">Project Complete</p>
+                    </div>
+
+                    <div className="count-wrap fade-in-up 3xl:w-[42%] 2xl:w-[42%] xl:w-[40%] lg:w-[43%] md:w-[40%] sm:w-[44%] max-xl:items-start items-center flex flex-col justify-center max-md:items-center">
+                        <h3 className="3xl:text-3xlh3 3xl:leading-3xlh3 2xl:text-2xlh3 2xl:leading-2xlh3 xl:text-xlh3 xl:leading-xlh3 lg:text-lgh3 lg:leading-lgh3 md:text-mdh3 md:leading-mdh3 sm:text-smh3 sm:leading-smh3">99%</h3>
+                        <p className="3xl:text-3xlp 3xl:leading-3xlp 2xl:text-2xlp 2xl:leading-2xlp xl:text-xlp xl:leading-xlp lg:text-lgp lg:leading-lgp md:text-mdp md:leading-mdp sm:text-smp sm:leading-smp">Satisfied Clients</p>
+                    </div>
+
+                    <div className="count-wrap fade-in-up 3xl:w-[42%] 2xl:w-[42%] xl:w-[40%] lg:w-[43%] md:w-[40%] sm:w-[44%] max-xl:items-start items-center flex flex-col justify-center max-md:items-center">
+                        <h3 className="3xl:text-3xlh3 3xl:leading-3xlh3 2xl:text-2xlh3 2xl:leading-2xlh3 xl:text-xlh3 xl:leading-xlh3 lg:text-lgh3 lg:leading-lgh3 md:text-mdh3 md:leading-mdh3 sm:text-smh3 sm:leading-smh3">6240+</h3>
+                        <p className="3xl:text-3xlp 3xl:leading-3xlp 2xl:text-2xlp 2xl:leading-2xlp xl:text-xlp xl:leading-xlp lg:text-lgp lg:leading-lgp md:text-mdp md:leading-mdp sm:text-smp sm:leading-smp">Hours of work</p>
+                    </div>
+
+                    <div className="count-wrap fade-in-up 3xl:w-[42%] 2xl:w-[42%] xl:w-[40%] lg:w-[43%] md:w-[40%] sm:w-[44%] max-xl:items-start items-center flex flex-col justify-center max-md:items-center">
+                        <h3 className="3xl:text-3xlh3 3xl:leading-3xlh3 2xl:text-2xlh3 2xl:leading-2xlh3 xl:text-xlh3 xl:leading-xlh3 lg:text-lgh3 lg:leading-lgh3 md:text-mdh3 md:leading-mdh3 sm:text-smh3 sm:leading-smh3">05+</h3>
+                        <p className="3xl:text-3xlp 3xl:leading-3xlp 2xl:text-2xlp 2xl:leading-2xlp xl:text-xlp xl:leading-xlp lg:text-lgp lg:leading-lgp md:text-mdp md:leading-mdp sm:text-smp sm:leading-smp">Years Experience</p>
+                    </div>
+                </div>
+            </div>
+
+            <div className="logo-images flex items-center justify-center gap-x-8 gap-y-4 py-2 flex-wrap">
+                <div className="images max-md:w-[30%] lg:w-[28%] xl:w-[21%] 2xl:w-[18%] 3xl:w-[17%]">
                     <a href="https://itscholars.in/"><img className="size-full" src="./images/IT Scholars.png" loading="lazy" alt="" /></a>
                 </div>
-                <div className="images w-[17%]">
+                <div className="images max-md:w-[30%] lg:w-[28%] xl:w-[21%] 2xl:w-[18%] 3xl:w-[17%]">
                     <a href="https://sutariyainfotech.com/"><img className="size-full" src="./images/Sutariya Infotech.png" loading="lazy" alt="" /></a>
                 </div>
-                <div className="images w-[17%]">
+                <div className="images max-md:w-[30%] lg:w-[28%] xl:w-[21%] 2xl:w-[18%] 3xl:w-[17%]">
                     <a href="https://digiinvitation.com/"><img className="size-full" src="./images/Digi Invitation.png" loading="lazy" alt="" /></a>
                 </div>
-                <div className="images w-[17%]">
+                <div className="images max-md:w-[30%] lg:w-[28%] xl:w-[21%] 2xl:w-[18%] 3xl:w-[17%]">
                     <a href="https://uniprint.in/"><img className="size-full" src="./images/Uniprint.png" loading="lazy" alt="" /></a>
                 </div>
-                <div className="images w-[17%]">
+                <div className="images max-md:w-[30%] lg:w-[28%] xl:w-[21%] 2xl:w-[18%] 3xl:w-[17%]">
                     <a href="#"><img className="size-full" src="./images/Marketing 365.png" loading="lazy" alt="" /></a>
                 </div>
             </div>
@@ -67,3 +72,19 @@ const AboutUs = () => {
 
 
 export default AboutUs;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
